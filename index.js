59ede05f0c909069b6edc79a4c05c69313e8f1a8
JustARatherRidiculouslyLongUsername <viswas.b10@gmail.com>
2020-10-18 19:47:10 +0400

    test child_process

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const { spawn } = require('child_process')
+
+
+app.get('/', (req, res) => { res.send('cool') })
+
+app.get('/:a/:b', (req, res) => {
+	let {a, b} = req.params
+	
+	const python_process = spawn('python', ['./x.py', a, b])
+
+	python_process.stdout.on('data', data => {
+		console.log('python says ', data)
+		res.end(data.toString())
+	})
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at port ${port}`)
+  console.log(`listening at port ${port}`)
 })
---
+++ x.py
@@ -0,0 +1,5 @@
+import sys
+
+a, b = sys.argv[1:]
+
+print('boom', int(a) ** int(b))
